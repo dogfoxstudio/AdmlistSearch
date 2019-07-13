09225048c216b4988932dcc68cafd8c682fb655f
dogfoxstudio <dogfox8@yandex.ru>
2019-07-13 14:35:15 +0300

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -1,12 +1,20 @@
 import pandas as pd
+import sys
 import admsearch
 
-with open('listabit.txt', 'r') as al:
-    names = al.read().splitlines()
+names = []
+
+if (len(sys.argv)>1):
+    for i in range(1, len(sys.argv)):
+        names.append(sys.argv[i])
+else:
+    with open('listabit.txt', 'r') as la:
+        names = la.read().splitlines()
 html = ""
 for name in names:
+    html = html + "<h3>" + name + "</h3>"
     table = admsearch.search_by_name(name)
     html += pd.DataFrame(table).to_html(header=None, index=False, border=2)
 
-with open('list.html', 'w') as f:
+with open('a.html', 'w') as f:
     f.write(html)
