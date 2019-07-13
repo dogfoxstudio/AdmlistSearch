af47c08a500a4777e46f5bd0c46820cc73229817
dogfoxstudio <dogfox8@yandex.ru>
2019-07-13 12:36:14 +0300

    Update and rename visualize.py to visualiыe.py

--- visualiыe.py
+++ visualiыe.py
@@ -8,5 +8,5 @@ for name in names:
     table = admsearch.search_by_name(name)
     html += pd.DataFrame(table).to_html(header=None, index=False, border=2)
 
-with open('a.html', 'w') as f:
+with open('list.html', 'w') as f:
     f.write(html)
